ed5d1586fe3738ddef22d13512d0329809b49b6e
ABDULLAHI OLADIMEJI <b-aoladimeji@microsoft.com>
2024-07-01 01:02:26 +0100

    Start auth

--- Trips/ClientApp/src/components/NavMenu.js
+++ Trips/ClientApp/src/components/NavMenu.js
@@ -5,46 +5,80 @@ import './NavMenu.css';
 import { useAuth0 } from '../auth0-wrapper';
 
 const NavMenu = () => {
-  console
-}
-export class NavMenu extends Component {
-  static displayName = NavMenu.name;
-
-  constructor (props) {
-    super(props);
-
-    this.toggleNavbar = this.toggleNavbar.bind(this);
-    this.state = {
-      collapsed: true
-    };
-  }
-
-  toggleNavbar () {
-    this.setState({
-      collapsed: !this.state.collapsed
-    });
-  }
-
-  render () {
-    return (
-      <header>
-        <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
-          <Container>
-            <NavbarBrand tag={Link} to="/">Trips</NavbarBrand>
-            <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
-            <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
-              <ul className="navbar-nav flex-grow">
+  const {isAuthenticated, loginWithRedirect, logout}= useAuth0();
+  return (
+    <header>
+      <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
+        <Container>
+          <NavbarBrand tag={Link} to="/">Trips</NavbarBrand>
+          {isAuthenticated ? (
+            <ul className="navbar-nav flex-grow">
+            <NavItem>
+                <NavLink tag={Link} className="text-dark" to="/create">Create</NavLink>
+              </NavItem>
               <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/create">Create</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/trips">Trips</NavLink>
-                </NavItem>
-              </ul>
-            </Collapse>
-          </Container>
-        </Navbar>
-      </header>
-    );
-  }
+                <NavLink tag={Link} className="text-dark" to="/trips">Trips</NavLink>
+              </NavItem>
+              <NavItem>
+                <button className="btn btn-danger" onClick={()=> logout()} >Log out</button>
+              </NavItem>
+            </ul>
+
+          ) : (
+            
+            <ul className="navbar-nav flex-grow">
+            <NavItem>
+                <button className="btn btn-success" onClick={()=> loginWithRedirect()} >Log in</button>
+              </NavItem>
+
+            </ul>
+
+          )}
+            
+        </Container>
+      </Navbar>
+    </header>
+  );
 }
+export default NavMenu
+// export class NavMenu extends Component {
+//   static displayName = NavMenu.name;
+
+//   constructor (props) {
+//     super(props);
+
+//     this.toggleNavbar = this.toggleNavbar.bind(this);
+//     this.state = {
+//       collapsed: true
+//     };
+//   }
+
+//   toggleNavbar () {
+//     this.setState({
+//       collapsed: !this.state.collapsed
+//     });
+//   }
+
+//   render () {
+//     return (
+//       <header>
+//         <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
+//           <Container>
+//             <NavbarBrand tag={Link} to="/">Trips</NavbarBrand>
+//             <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
+//             <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
+//               <ul className="navbar-nav flex-grow">
+//               <NavItem>
+//                   <NavLink tag={Link} className="text-dark" to="/create">Create</NavLink>
+//                 </NavItem>
+//                 <NavItem>
+//                   <NavLink tag={Link} className="text-dark" to="/trips">Trips</NavLink>
+//                 </NavItem>
+//               </ul>
+//             </Collapse>
+//           </Container>
+//         </Navbar>
+//       </header>
+//     );
+//   }
+// }
